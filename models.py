f0ad837053649616c07e46172758139805619bbb
Eddycrack 864 <89285504+Eddycrack864@users.noreply.github.com>
2024-06-17 21:10:13 -0500

    link fix

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ downloads = [
   ("https://huggingface.co/Eddycrack864/Music-Source-Separation-Training/resolve/main/model_mel_band_roformer_ep_3005_sdr_11.4360.yaml", f"{download_dir}/model_mel_band_roformer_ep_3005_sdr_11.4360.yaml"),
   ("https://huggingface.co/Eddycrack864/Music-Source-Separation-Training/resolve/main/mel_band_roformer_crowd_aufr33_viperx_sdr_8.7144.ckpt", f"{download_dir}/mel_band_roformer_crowd_aufr33_viperx_sdr_8.7144.ckpt"),
   ("https://huggingface.co/Eddycrack864/Music-Source-Separation-Training/resolve/main/model_mel_band_roformer_crowd.yaml", f"{download_dir}/model_mel_band_roformer_crowd.yaml"),
-  ("https://huggingface.co/Eddycrack864/Music-Source-Separation-Training/resolve/main/model_bandit_plus_dnr_sdr_11.47.chpt", f"{download_dir}/model_mel_band_roformer_crowd.yaml"),
+  ("https://huggingface.co/Eddycrack864/Music-Source-Separation-Training/resolve/main/model_bandit_plus_dnr_sdr_11.47.chpt", f"{download_dir}/model_bandit_plus_dnr_sdr_11.47.chpt"),
   ("https://huggingface.co/Eddycrack864/Music-Source-Separation-Training/resolve/main/config_dnr_bandit_bsrnn_multi_mus64.yaml", f"{download_dir}/config_dnr_bandit_bsrnn_multi_mus64.yaml"),
   ("https://huggingface.co/Eddycrack864/Music-Source-Separation-Training/resolve/main/model_vocals_segm_models_sdr_9.77.ckpt", f"{download_dir}/model_vocals_segm_models_sdr_9.77.ckpt"),
   ("https://huggingface.co/Eddycrack864/Music-Source-Separation-Training/resolve/main/config_vocals_segm_models.yaml", f"{download_dir}/config_vocals_segm_models.yaml"),
